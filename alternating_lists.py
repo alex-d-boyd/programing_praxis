13e3acd5bbd157856644a53e338acbf6d7a19d6f
Alex Boyd <alex.david.boyd@gmail.com>
2019-02-05 14:13:31 +0900

    Corrected algorithm

--- alternating_lists.py
+++ alternating_lists.py
@@ -3,11 +3,13 @@
 # <<Alternating Lists>>
 # <<https://programmingpraxis.com/2019/02/01/alternating-lists/>>
 
+from itertools import zip_longest
+
 def alternate(*lists):
-    pass
-
-
+    return [item for tup in zip_longest(*lists) for item in tup
+            if item is not None]
 
 def test():
-    assert alternate([1,2,3,4,5], ["a", "b", "c"], ["w", "x", "y", "z"]) ==
-    [1, "a", "w", 2, "b", "x", 3, "c", "y", 4, "z", 5]
+    l1, l2, l3 = [1,2,3,4,5], ["a", "b", "c"], ["w", "x", "y", "z"]
+    print(alternate(l1, l2, l3))
+    assert alternate(l1, l2, l3) == [1, "a", "w", 2, "b", "x", 3, "c", "y", 4, "z", 5]
